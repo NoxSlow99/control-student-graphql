ea4a8308c6d1a59564cf5f07940d579b6cd4e595
NoxSlow99 <angel_jcc99@outlook.com>
2024-01-20 17:24:18 -0500

    Subida de inicio del proyecto

---
+++ src/test/java/com/controlStudents/ControlStudentGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlStudentGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
